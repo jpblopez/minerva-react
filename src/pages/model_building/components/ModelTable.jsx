c05b8f5dfa043ff378919348d7d32430cadf9bff
Rhyan La Rosa <rhyan.larosa.20@usjr.edu.ph>
2021-11-13 10:55:32 +0800

    Added Clustering

--- src/pages/model_building/components/ModelTable.jsx
+++ src/pages/model_building/components/ModelTable.jsx
@@ -1,57 +1,51 @@
-import React from 'react'
-
+import React from 'react';
 
 const ModelTable = ({ data }) => {
-    const tempData = []
-    console.log(data)
+  const tempData = [];
+  console.log(data);
 
+  Object.keys(data).forEach((row) => {
+    Object.keys(data[row]).forEach((col) => {
+      const keywords = data[row][col];
+      if (keywords.length !== 0) {
+        tempData.push({ row, col, keywords });
+      }
+    });
+  });
 
-    Object.keys(data).forEach(row => {
-        Object.keys(data[row]).forEach(col => {
-            const keywords = data[row][col]
-            if (keywords.length !== 0) {
-                tempData.push({ row, col, keywords })
-            }
-        })
-    })
+  console.log(tempData);
+  return (
+    <>
+      <table className="scroll-table w-full">
+        <thead>
+          <td className="p-2 text-md">Cluster No.</td>
+          <td>Keywords</td>
+        </thead>
+        <tbody>
+          {tempData.map((item, index) => {
+            const container = `${
+              index % 2 === 0 ? 'bg-gray-100' : ''
+            } hover:bg-indigo-50 cursor-default`;
 
-    console.log(tempData)
-    return (
-        <>
-            <table className="scroll-table w-full">
-                <thead >
-                    <td className="p-2 text-md">
-                        Cluster No.
-                    </td>
-                    <td>
-                        Keywords
-                    </td>
-                </thead>
-                <tbody>
-                    {tempData.map((item, index) => {
-                        const container = `${index % 2 === 0 ? 'bg-gray-100' : ''
-                            } hover:bg-indigo-50 cursor-default`;
+            return (
+              <>
+                <tr className={container}>
+                  <td>
+                    ({item.row}) , ({item.col})
+                  </td>
+                  <td>
+                    {item.keywords.map((keyword) => (
+                      <>{keyword}, </>
+                    ))}
+                  </td>
+                </tr>
+              </>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
-                        return (
-                            <>
-                                <tr className={container}>
-                                    <td>
-                                        ({item.row}) ,
-                                        ({item.col})
-                                    </td>
-                                    <td>
-                                        {item.keywords.map((keyword) => (<>{keyword}, </>))}
-                                    </td>
-                                </tr>
-                            </>
-                        )
-                    }
-                    )}
-                </tbody>
-
-            </table>
-        </>
-    )
-}
-
-export default ModelTable
+export default ModelTable;
